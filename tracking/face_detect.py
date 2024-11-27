68389224725a39b6c29ac1760b5065e7910f362e
jiangziben <jiangziben@outlook.com>
2024-11-27 10:45:04 +0800

    Feat:1.增加人体特征识别功能

--- tracking/face_detect.py
+++ tracking/face_detect.py
@@ -11,7 +11,7 @@ from pathlib import PosixPath
 from tracking.face_location import get_face_location_from_keypoints,draw_face_bboxes
 import torch
 import time
-
+from boxmot.appearance.reid_auto_backend import ReidAutoBackend
 class FaceDetector:
     def __init__(self, host_image_path):
         # Load a sample picture and learn how to recognize it.
@@ -178,10 +178,7 @@ class FaceDetectorV3:
                             face_locations.append([top, right, bottom, left])
                     face_encodings = face_recognition.face_encodings(img,face_locations)
                     for face_encoding in face_encodings:
-                        if len(self.known_face_encodings) == 0:
-                            self.known_face_encodings = face_encoding[np.newaxis,:]
-                        else:
-                            self.known_face_encodings = np.concatenate((face_encoding[np.newaxis,:], self.known_face_encodings), axis=0)
+                        self.known_face_encodings = np.concatenate((face_encoding[np.newaxis,:], self.known_face_encodings), axis=0)
                     self.known_face_ids.append(i)
                 self.known_face_names.append(person_name)
             self.known_face_ids = self.known_face_ids[::-1]
@@ -201,7 +198,7 @@ class FaceDetectorV3:
             face_encoding = self.reid_model(face_image_tensor.unsqueeze(0))
             face_encodings.append(face_encoding[0])
         return face_encodings
-    
+
     def confidence_exponential(self,d, alpha=1.0):
         return np.exp(-alpha * d)
     
@@ -280,6 +277,176 @@ class FaceDetectorV3:
             face_confidences.append(face_confidence[0])
         return face_ids,face_locations_x1y1x2y2,face_encodings,face_confidences,person_indexes
 
+class FaceDetectorV4:
+    def __init__(self,detection_model,reid_model, host_image_path):
+        if isinstance(detection_model, str) or isinstance(detection_model, PosixPath):
+            self.detection_model = YOLO(detection_model)  # build from YAML and transfer weights
+        else:
+            self.detection_model = detection_model
+        if isinstance(reid_model, str) or isinstance(reid_model, PosixPath):
+            self.reid_model = ReidAutoBackend(
+                weights=PosixPath(reid_model), device=torch.device("cuda:0"), half=False
+            ).model
+        else:
+            self.reid_model = reid_model
+        self.known_face_encodings = []
+        self.known_face_names = []
+        self.known_face_ids = []
+        self.known_body_encodings = []
+        # 检查文件是否存在
+        query_file_path = os.path.join(host_image_path, 'query_features.npy')
+        names_file_path = os.path.join(host_image_path, 'names.npy')
+        ids_file_path = os.path.join(host_image_path, 'ids.npy')
+        query_body_file_path = os.path.join(host_image_path, 'query_body_features.npy')
+        if os.path.exists(query_file_path) and os.path.exists(names_file_path) and os.path.exists(ids_file_path) and os.path.exists(query_body_file_path):
+            self.known_face_encodings = np.load(query_file_path)
+            self.known_face_ids = np.load(ids_file_path)
+            self.known_face_names = np.load(names_file_path)
+            self.known_body_encodings = np.load(query_body_file_path)
+            print("Files loaded successfully.")
+        else:
+            print("One or both files do not exist.")
+            self.known_face_encodings = np.ones((1,128), dtype=np.int64)
+            self.known_body_encodings = np.ones((1,512), dtype=np.int64)
+            # print(q, q.shape)
+            for i,person_name in enumerate(os.listdir(host_image_path)):
+                if not os.path.isdir(os.path.join(host_image_path, person_name)):
+                    continue
+                for image_name in os.listdir(os.path.join(host_image_path, person_name)):
+                    img = face_recognition.load_image_file(os.path.join(host_image_path, person_name, image_name))
+                    people_keypoints = self.detection_model.predict(img)[0].keypoints.data
+                    people_locations = self.detection_model.predict(img)[0].boxes.data
+                    face_locations = []
+                    for person_keypoints in people_keypoints:
+                        face_bbox = get_face_location_from_keypoints(person_keypoints,img.shape)                        
+                        if face_bbox is not None:
+                            (left, top, right, bottom) = map(int,face_bbox)
+                            face_locations.append([top, right, bottom, left])
+                    face_encodings = face_recognition.face_encodings(img,face_locations)
+                    body_encodings = self.reid_model.get_features(people_locations[:,:4].cpu().numpy(),img)
+                    for face_encoding, body_encoding in zip(face_encodings,body_encodings):
+                        self.known_face_encodings = np.concatenate((face_encoding[np.newaxis,:], self.known_face_encodings), axis=0)
+                        self.known_body_encodings = np.concatenate((body_encoding[np.newaxis,:], self.known_body_encodings), axis=0)
+                    self.known_face_ids.append(i)
+                self.known_face_names.append(person_name)
+            self.known_face_ids = self.known_face_ids[::-1]
+            self.known_face_ids.append(-1)
+            np.save(os.path.join(host_image_path, 'query_features'), self.known_face_encodings)
+            np.save(os.path.join(host_image_path, 'query_body_features'), self.known_body_encodings)
+            np.save(os.path.join(host_image_path, 'ids'), self.known_face_ids)  # save query
+            np.save(os.path.join(host_image_path, 'names'), self.known_face_names)  # save query
+
+    def face_encodings(self,img,face_locations):
+        face_encodings = []
+        for face_location in face_locations:
+            left, top, right, bottom = face_location
+            face_image = img[int(top):int(bottom), int(left):int(right)]
+            face_image = cv2.resize(face_image, (640, 640))
+            face_image_tensor = torch.from_numpy(face_image).float()
+            face_image_tensor = face_image_tensor.permute(2, 0, 1)
+            face_encoding = self.reid_model(face_image_tensor.unsqueeze(0))
+            face_encodings.append(face_encoding[0])
+        return face_encodings
+
+    def confidence_exponential(self,d, alpha=1.0):
+        return np.exp(-alpha * d)
+    
+    def detect_faces(self, unknown_image, threshold = 0.7):
+    
+        # Find all the faces and face encodings in the unknown image
+        people_keypoints = self.detection_model.predict(unknown_image,verbose=False)[0].keypoints.data
+        person_indexes = []
+        face_locations = []
+        for i,person_keypoints in enumerate(people_keypoints):
+            face_bbox = get_face_location_from_keypoints(person_keypoints,unknown_image.shape)
+            if face_bbox is not None:
+                (left, top, right, bottom) = map(int,face_bbox)
+                face_locations.append([top, right, bottom, left])
+                person_indexes.append(i)
+        face_encodings = face_recognition.face_encodings(unknown_image,face_locations)
+        face_ids = []
+        face_locations_x1y1x2y2 = []
+        face_confidences = []
+        # Loop through each face found in the unknown image
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            # See if the face is a match for the known face(s)
+            # cos_sim = cosine_similarity(face_encoding[np.newaxis, :], self.known_face_encodings)
+            # max_idx = np.argmax(cos_sim, axis=1)
+            # maximum = np.max(cos_sim, axis=1)
+            # max_idx[maximum < threshold] = -1
+            # face_id = self.known_face_ids[max_idx[0]]
+            euclidean_sim = euclidean_distances(face_encoding[np.newaxis, :], self.known_face_encodings)
+            min_idx = np.argmin(euclidean_sim, axis=1)
+            minimun = np.min(euclidean_sim, axis=1)
+            face_confidence = self.confidence_exponential(minimun)
+            min_idx[face_confidence < threshold] = -1
+            if min_idx[0] >= 0:
+                face_id = self.known_face_ids[min_idx[0]] 
+            else:
+                face_id = -1
+            face_ids.append(face_id)
+            face_locations_x1y1x2y2.append([left, top, right, bottom])
+            face_confidences.append(face_confidence[0])
+        return face_ids,face_locations_x1y1x2y2,face_encodings,face_confidences
+    
+    def detect_faces(self, unknown_image,detect_result, threshold = 0.7):
+        people_keypoints = detect_result.keypoints.data
+        # Find all the faces and face encodings in the unknown image
+        person_indexes = []
+        face_locations = []
+        for i,person_keypoints in enumerate(people_keypoints):
+            face_bbox = get_face_location_from_keypoints(person_keypoints,unknown_image.shape)
+            if face_bbox is not None:
+                (left, top, right, bottom) = map(int,face_bbox)
+                face_locations.append([top, right, bottom, left])
+                person_indexes.append(i)
+        face_encodings = face_recognition.face_encodings(unknown_image,face_locations)
+        face_ids = []
+        face_locations_x1y1x2y2 = []
+        face_confidences = []
+        # Loop through each face found in the unknown image
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            # See if the face is a match for the known face(s)
+            # cos_sim = cosine_similarity(face_encoding[np.newaxis, :], self.known_face_encodings)
+            # max_idx = np.argmax(cos_sim, axis=1)
+            # maximum = np.max(cos_sim, axis=1)
+            # max_idx[maximum < threshold] = -1
+            # face_id = self.known_face_ids[max_idx[0]]
+            euclidean_sim = euclidean_distances(face_encoding[np.newaxis, :], self.known_face_encodings)
+            min_idx = np.argmin(euclidean_sim, axis=1)
+            minimun = np.min(euclidean_sim, axis=1)
+            face_confidence = self.confidence_exponential(minimun)
+            min_idx[face_confidence < threshold] = -1
+            if min_idx[0] >= 0:
+                face_id = self.known_face_ids[min_idx[0]] 
+            else:
+                face_id = -1
+            face_ids.append(face_id)
+            face_locations_x1y1x2y2.append([left, top, right, bottom])
+            face_confidences.append(face_confidence[0])
+        return face_ids,face_locations_x1y1x2y2,face_encodings,face_confidences,person_indexes
+    
+    def detect_bodys(self, unknown_image,detect_result, threshold = 0.5):
+        people_locations = detect_result.boxes.data
+        # Find all the faces and face encodings in the unknown image
+        body_encodings = self.reid_model.get_features(people_locations[:,:4].cpu().numpy(), unknown_image)
+        face_ids = []
+        body_confidences = []
+        # Loop through each face found in the unknown image
+        for body_encoding in body_encodings:
+            euclidean_sim = euclidean_distances(body_encoding[np.newaxis, :], self.known_body_encodings)
+            min_idx = np.argmin(euclidean_sim, axis=1)
+            minimun = np.min(euclidean_sim, axis=1)
+            body_confidence = self.confidence_exponential(minimun)
+            min_idx[body_confidence < threshold] = -1
+            if min_idx[0] >= 0:
+                face_id = self.known_face_ids[min_idx[0]] 
+            else:
+                face_id = -1
+            face_ids.append(face_id)
+            body_confidences.append(body_confidence[0])
+        return face_ids,people_locations,body_encodings,body_confidences
+
 
 def get_images_in_folder(folder_path):
     """
@@ -300,12 +467,12 @@ def get_images_in_folder(folder_path):
 if __name__ == "__main__":
     # This is an example of running face recognition on a single image
     # and drawing a box around each person that was identified.
-    face_detector = FaceDetectorV3("/home/jiangziben/CodeProject/boxmot/tracking/weights/yolov11m-pose.pt",
-                                   "/home/jiangziben/CodeProject/boxmot/tracking/weights/20180402-114759-vggface2.pt",
+    face_detector = FaceDetectorV4("/home/jiangziben/CodeProject/boxmot/tracking/weights/yolov11m-pose.pt",
+                                   "/home/jiangziben/CodeProject/boxmot/tracking/weights/osnet_x0_25_msmt17.pt",
                                    "/home/jiangziben/CodeProject/boxmot/data/known_people/")
     # face_detector = FaceDetector("/home/jiangziben/data/people_tracking/known_people/jzb/jzb.png")
     # Load an image with an unknown face
-    folder_path = "/home/jiangziben/data/people_tracking/multi_people"
+    folder_path = "/home/jiangziben/data/people_tracking/multi_people/"
     person_name = folder_path.split("/")[-1]
     images_path = get_images_in_folder(folder_path)
     num_all = len(images_path)
@@ -317,6 +484,7 @@ if __name__ == "__main__":
         detect_result = face_detector.detection_model.predict(unknown_image,verbose=False)[0]
         people_bboxes = detect_result.boxes.data
         face_ids,face_locations,face_encodings,face_confidences,person_indexes = face_detector.detect_faces(unknown_image,detect_result)
+        body_ids,body_locations,body_encodings,body_confidences = face_detector.detect_bodys(unknown_image,detect_result)
         print("used_times: ",time.time() - start)
         # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
         # See http://pillow.readthedocs.io/ for more about PIL/Pillow
@@ -328,11 +496,20 @@ if __name__ == "__main__":
         for face_id, face_location, face_encoding,face_confidence,person_index in zip(face_ids, face_locations, face_encodings,face_confidences,person_indexes):
             left, top, right, bottom = face_location
             person_box = people_bboxes[person_index]
-            
             if face_id == -1:
                 name = "Unknown"
             else:
                 name = face_detector.known_face_names[face_id]
+            
+            body_confidence = 0.0
+            body_name = "Unknown"
+            body_id = body_ids[person_index]
+            body_confidence = body_confidences[person_index]            
+            if body_id == -1:
+                body_name = "Unknown"
+            else:
+                body_name = face_detector.known_face_names[body_id]
+            
             if(person_name == name):
                 num_right += 1
             
@@ -344,7 +521,7 @@ if __name__ == "__main__":
             # # Draw a label with a name below the face
             text_width, text_height = 50,50
             # draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-            draw.text((left + 6, bottom - text_height - 5), name + ", c: " + f"{face_confidence:.2f}", fill=(255, 0, 0, 255))
+            draw.text((left + 6, bottom - text_height - 5), name + ", c: " + f"{face_confidence:.2f}  " + body_name + ", c: " + f"{body_confidence:.2f}", fill=(255, 0, 0, 255))
 
         # Remove the drawing library from memory as per the Pillow docs
         del draw
